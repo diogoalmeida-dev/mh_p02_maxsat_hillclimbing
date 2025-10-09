85a968f2086b90b6bd5908f3afe8c70ab6f1d835
a81477 <a81477@ualg.pt>
2025-10-09 19:35:38 +0100

    next ascent hillclimbing implemented

---
+++ src/p02_1.py
@@ -0,0 +1,125 @@
+## function to read the CNF from a DIMACS file
+import itertools
+import random
+import time
+
+# iterations of the algorithm
+num_runs = 30
+
+# functions that reads the cnf file and stores variables and lists of its content
+def read_cnf():
+    clauses = []
+    num_clauses = 0
+    num_vars = 0
+
+    with open("../cnf_files/uf20-01.cnf", "r") as f:
+        for line in f:
+            line = line.strip()
+            if not line or line.startswith("c"):
+                continue
+
+            if line.startswith("p"): # process header line (starts with p)
+                parts = line.split()
+                num_vars = int(parts[2])
+                num_clauses = int(parts[3])
+                continue
+
+            clause = []             # clause lines, finish with 0
+            for x in line.split():
+                if x == '0': ## if 0, go next line
+                    continue
+                try:
+                    clause.append(int(x)) ## if not append the integer
+                except ValueError: ## if any char besides integers, skip line (bcs of % 0 in end of file .cnf)
+                    continue
+
+            if clause:
+                clauses.append(clause)
+
+            if len(clauses) >= num_clauses: ## stop after all clauses are read
+                break
+
+    return clauses, num_clauses, num_vars
+
+## function to find all combinations of true/false assignments for variables
+def random_combination(num_vars):
+    combinations = list(itertools.product([1, 0], repeat=num_vars))
+
+    return random.choice(combinations) ## get a random combination of true and false assignments to the variables
+
+# Function to find the fitness being the fitness the number of clauses satisfied by the combination
+def evaluate_fitness(clauses, combination):
+    fitness = 0
+
+    for clause in clauses:
+
+        clause_fitness = 0
+        for value in clause:
+            var_index = abs(value) - 1 ## normalise for clause list
+            bool_value = combination[var_index]
+            if value < 0: ## if literal value is below 0, the variable is negated
+                bool_value = not bool_value
+            if bool_value: ## if bool is true
+                clause_fitness = 1
+                break
+        fitness += clause_fitness
+    return fitness
+
+## generates the neighbour of the current solution by flipping a bit on each value of the combination
+def generate_neighbours(combination):
+    neighbours = []
+    for var_index in range (len(combination)): ## indexar cada true e false
+        tmp_combination = list(combination) ## tran
+        bool_value = (combination[var_index] + 1) % 2
+        tmp_combination[var_index] = bool_value
+        neighbours.append(tmp_combination)
+
+    return neighbours
+
+## implements next ascent hillclimbing using 1 bit hamming distance neighbourhood
+def hillclimbing():
+    clauses, num_clauses, num_vars = read_cnf() ## coletar o conteúdo do ficheiro
+
+    initial_solution = random_combination(num_vars) ## começar por uma solução random
+    fitness = evaluate_fitness(clauses, initial_solution) ## descobrir o fitness
+
+    tmp_solution = initial_solution
+
+    evaluations = 1 ## contagem da primeira avaliacao
+
+    start_time = time.process_time()
+
+    while True:
+        if fitness == num_clauses: ## early optimal stop
+            break
+
+        neighbours = generate_neighbours(tmp_solution)
+        better_found = False
+
+        for neighbour in neighbours:
+            nb_fitness = evaluate_fitness(clauses, neighbour)
+            evaluations += 1
+
+            if nb_fitness > fitness:
+                fitness = nb_fitness
+                tmp_solution = neighbour
+                better_found = True
+                break
+        if not better_found:
+            break
+
+    cpu_time = time.process_time() - start_time
+    return tmp_solution, fitness, evaluations, cpu_time
+
+def main():
+    for x in range(num_runs):
+        solution, fitness, evals, cpu = hillclimbing()
+
+        if x < 9:
+            print(f"Run 0{x + 1}: Best fitness = {fitness}, Evaluations = {evals}, CPU time = {cpu:.4f} s")
+            continue
+        print(f"Run {x + 1}: Best fitness = {fitness}, Evaluations = {evals}, CPU time = {cpu:.4f} s")
+
+
+if __name__ == "__main__":
+    main()
